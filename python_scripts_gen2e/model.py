1aad9bccf76df9af3e8deb030d5fd5e97bfa7905
Gonzalo Mantiñan Suárez <g.mantinan@alumnos.upm.es>
2025-01-02 21:44:18 +0100

    mmmm igual hay suerte

--- python_scripts_gen2e/model.py
+++ python_scripts_gen2e/model.py
@@ -25,20 +25,16 @@ class VariationalEncoder(nn.Module):
         # self.batch5 = nn.BatchNorm2d(32*2)
         # self.conv6 = nn.Conv2d(in_channels=32*2, out_channels=32, kernel_size=(1, 1), stride=(1, 1))
 
-        #OJO - recurrent encoder
-        self.conv1 = nn.Conv2d(in_channels=2*2, out_channels=512*2, kernel_size=(5, 5), stride=(2, 2)) # Voy a concatenar los datos de entrada en la dimension de bines de frecuencia (512->1024)... a ver qué tal!
-        self.batch1 = nn.BatchNorm2d(512*2) # BatchNorm2d => Preguntar por la expresión de la documentación de PyTorch
-        self.conv2 = nn.Conv2d(in_channels=512*2, out_channels=256*2, kernel_size=(3, 3), stride=(2, 2))
-        self.batch2 = nn.BatchNorm2d(256*2)
-        self.conv3 = nn.Conv2d(in_channels=256*2, out_channels=128*2, kernel_size=(3, 3), stride=(2, 2))
-        self.batch3 = nn.BatchNorm2d(128*2)
-
-        self.flatten = nn.Flatten() #como no podemos asegurar que mantenga la secuencialidad en las muestras tras haber pasado
-        self.lstm = nn.LSTM(input_size=256 * 62 *6, hidden_size = 32 * 31 * 3, num_layers = 3, batch_first=True)
+        # LSTM para capturar dependencias temporales
+        # La entrada será de tamaño (batch_size, secuencia_temporal, 4 * 512) = (batch_size, 57, 2048)
+        self.lstm = nn.LSTM(input_size = 4*512, hidden_size = 256,num_layers= 1, batch_first=True)
 
         # distribution parameters
-        self.mu = nn.Linear(32 * 31 * 3, latent_dim)
-        self.var = nn.Linear(32 * 31 * 3, latent_dim)
+        # self.mu = nn.Linear(32 * 31 * 3, latent_dim)
+        # self.var = nn.Linear(32 * 31 * 3, latent_dim)
+
+        self.mu = nn.Linear(256, latent_dim)
+        self.var = nn.Linear(256, latent_dim)
 
         self.N = torch.distributions.Normal(0, 1)
         #self.N.loc = self.N.loc.cuda()  # hack to get sampling on the GPU
@@ -47,29 +43,28 @@ class VariationalEncoder(nn.Module):
 
     def forward(self, x):
         x = x.to(device)
-        
         # x = F.relu(self.batch1(self.conv1(x))) # Capa 1
         # x = F.relu(self.batch2(self.conv2(x))) # Capa 2
         # x = F.relu(self.batch3(self.conv3(x))) # Capa 3
         # x = F.relu(self.batch4(self.conv4(x))) # Capa 4
         # x = F.relu(self.batch5(self.conv5(x))) # Capa 5
         # x = self.conv6(x)                      # Capa 6 
+        # x = torch.flatten(x, start_dim=1) # Convertimos a un vector unidimensional
+        #mu = self.mu(x)
 
-        x = F.relu(self.batch1(self.conv1(x))) # Capa 1
-        x = F.relu(self.batch2(self.conv2(x))) # Capa 2
-        x = F.relu(self.batch3(self.conv3(x))) # Capa 3
+        # Aplanar las dimensiones de canales y frecuencias (4, 512) => 2048
+        x = x.view(x.size(0), x.size(3), -1)  # Forma (batch_size, secuencia_temporal, 2048)
+        # Procesar la secuencia temporal con LSTM
+        lstm_out, (h_n, c_n) = self.lstm(x)
+         # Usar la salida del último paso de tiempo para obtener mu y sigma
+        h_n = h_n[-1] 
+        mu = self.mu(h_n)                        # Capa '7'
+        sigma = torch.exp(self.var(h_n)).to(device)
 
-        x = self.flatten(x)  # Aplanar para la LSTM
-        x = x.unsqueeze(1)  # Añadir una dimensión "secuencial" ficticia (secuencia única)
-        x, _ = self.lstm(x)  # Pasar por LSTM
-        x = x.squeeze(1)  # Quitar la dimensión "secuencial" después de la LSTM
-
-        x = torch.flatten(x, start_dim=1) # Convertimos a un vector unidimensional
-        mu = self.mu(x)                        # Capa '7'
-        sigma = torch.exp(self.var(x)).to(device)
         z = self.N.sample(mu.shape).to(device)      # Capa '7'
         z = mu + sigma*z#self.N.sample(mu.shape) # ¿Qué es mu.shape? => Tamaño multidimensional de la media (mu)
         self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum() # Y esta forma de calcular la divergencia KL? => Es la expresión analítica de la dkl para dos gaussianas 
+        
         return z
     
 # Esto para cargar el valor de x del modelo de la gen1
@@ -86,28 +81,30 @@ class Decoder(nn.Module):
     def __init__(self, latent_dims):
         super().__init__()
 
-        # self.decoder_lin = nn.Sequential( # Asumo que nn.Sequential() define una serie de funciones que se aplicarán una tras otra 
-        #     nn.Linear(latent_dims, 128),
-        #     nn.ReLU(True), # ¿Qué significa el parámetro True en esta función ReLU?
-        #     nn.Linear(128, 32 * 31 * 3),
-        #     nn.ReLU(True)
-        # )
+        # Capa LSTM para procesar la representación latente
+        self.lstm = nn.LSTM(latent_dims, hidden_size=256, num_layers= 1,batch_first=True)
 
-        # Fully connected layers to reshape the latent vector
-        self.decoder_lin = nn.Sequential(
-            nn.Linear(latent_dims, 32 * 31 * 3),
+        # Capa lineal para reconstruir la salida de la LSTM
+        self.decoder_lin = nn.Sequential( # Asumo que nn.Sequential() define una serie de funciones que se aplicarán una tras otra 
+            nn.Linear(256, 128),
+            nn.ReLU(True), # ¿Qué significa el parámetro True en esta función ReLU?
+            nn.Linear(128, 2*2*512*57),
             nn.ReLU(True)
         )
 
-        self.lstm = nn.LSTM(input_size=32 * 31 * 3, hidden_size=256 * 62 * 6, num_layers=3, batch_first=True)
+        # self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 31, 3))
 
-        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(128*2, 62, 6))
-
-        self.dec0 = nn.ConvTranspose2d(in_channels=128*2, out_channels=256*2, kernel_size=(3, 3), stride=(2, 2), output_padding=1)
-        self.batch0 = nn.BatchNorm2d(256*2)
-        self.dec1 = nn.ConvTranspose2d(in_channels=256*2, out_channels=512*2, kernel_size=(3, 3), stride=(2, 2), output_padding=1)
-        self.batch1 = nn.BatchNorm2d(512*2)
-        self.dec2 = nn.ConvTranspose2d(in_channels=512*2, out_channels=2*2, kernel_size=(5, 5), stride=(2, 2), output_padding=1)
+        # self.dec0 = nn.ConvTranspose2d(in_channels=32, out_channels=32*2, kernel_size=(1, 1), stride=(1, 1))
+        # self.batch0 = nn.BatchNorm2d(32*2)
+        # self.dec1 = nn.ConvTranspose2d(in_channels=32*2, out_channels=64*2, kernel_size=(1, 1), stride=(1, 1)) # ¿ConvTranspose2d?
+        # self.batch1 = nn.BatchNorm2d(64*2)
+        # self.dec2 = nn.ConvTranspose2d(in_channels=64*2, out_channels=128*2, kernel_size=(2, 2), stride=(2, 2))
+        # self.batch2 = nn.BatchNorm2d(128*2)
+        # self.dec3 = nn.ConvTranspose2d(in_channels=128*2, out_channels=256*2, kernel_size=(3, 3), stride=(2, 2), output_padding=1)
+        # self.batch3 = nn.BatchNorm2d(256*2)
+        # self.dec4 = nn.ConvTranspose2d(in_channels=256*2, out_channels=512*2, kernel_size=(3, 3), stride=(2, 2), output_padding=1)
+        # self.batch4 = nn.BatchNorm2d(512*2)
+        # self.dec5 = nn.ConvTranspose2d(in_channels=512*2, out_channels=2*2, kernel_size=(5, 5), stride=(2, 2), output_padding=1)
 
     def forward(self, x):
         # x = self.decoder_lin(x)
@@ -121,20 +118,20 @@ class Decoder(nn.Module):
         # x = torch.sigmoid(x) # ¿Y esta última función de activación? ¿Es acaso para normalizar el resultado?
         # return x
 
-        x = self.decoder_lin(x)
+        # x tiene la forma (batch_size, latent_dim), necesitamos convertirlo en una secuencia de longitud 57.
+        x = x.unsqueeze(1).repeat(1, 57, 1)  # Añadir una dimensión de secuencia (batch_size, 1, latent_dim)
+        
+        # Pasamos el vector latente por el LSTM
+        lstm_out = self.lstm(x)
 
-        x = x.unsqueeze(1) #Añadimos artificialmente la dimension de seq_length
-        x, _ = self.lstm(x)
-        x = x.squeeze(1)
-        x = self.unflatten(x)
+    
+        # Usamos lstm_out, ya que contiene la secuencia completa de salidas del LSTM
+        x_reconstructed = self.decoder_lin(lstm_out)
 
-        x = F.relu(self.batch0(self.dec0(x)))
-        x = F.relu(self.batch1(self.dec1(x)))
-        x = self.dec2(x)
+        # Reorganizamos los datos para la forma deseada (batch_size, 4, 512, 57)
+        x_reconstructed = x_reconstructed.view(-1, 4, 512, 57)
 
-        x = torch.sigmoid(x)
-
-        return x
+        return x_reconstructed
 
 class VariationalAutoencoder(nn.Module):
     def __init__(self, latent_dims):
